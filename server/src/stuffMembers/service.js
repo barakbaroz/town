e51c65d97e6e58e81e8e7ba95ff05392dafdbc18
tomer255 <tomer255255@gmail.com>
2023-06-18 14:40:56 +0300

    Change info to casesCount

--- server/src/stuffMembers/service.js
+++ server/src/stuffMembers/service.js
@@ -1,16 +1,11 @@
-const { Cases, StaffMembers } = require("../models");
+const { Cases } = require("../models");
 const { Op, fn, col } = require("sequelize");
 
-module.exports.info = async ({ staffMembersId }) => {
+module.exports.casesCount = async ({ staffMembersId, role }) => {
   const startOfToday = new Date();
   startOfToday.setHours(0, 0, 0, 0);
   const endOfToday = new Date();
   endOfToday.setHours(23, 59, 59, 999);
-
-  const StaffMembersInfo = await StaffMembers.findOne({
-    where: { id: staffMembersId },
-    attributes: ["name", "role"],
-  });
 
   const totalCount = await Cases.findOne({
     where: { creatorId: staffMembersId },
@@ -27,6 +22,5 @@ module.exports.info = async ({ staffMembersId }) => {
   return {
     totalCases: totalCount.dataValues.count,
     todayCases: todayCount.dataValues.count,
-    ...StaffMembersInfo.dataValues,
   };
 };
