f62ff9d5b755b7792bb7e13aed97445d315da786
digmi123 <65775879+digmi123@users.noreply.github.com>
2023-07-02 13:41:20 +0300

    Remove unnecessary property.

--- client/src/components/JumpingPopup.jsx
+++ client/src/components/JumpingPopup.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 
-function JumpingPopup({ children, open, setOpen }) {
+function JumpingPopup({ children, open }) {
   useEffect(() => {
     if (open) {
       document.body.style.overflow = "hidden";
@@ -22,7 +22,6 @@ export default JumpingPopup;
 JumpingPopup.propTypes = {
   children: PropTypes.node,
   open: PropTypes.bool,
-  setOpen: PropTypes.func,
 };
 
 const ScreenWrapper = styled.div`
@@ -38,20 +37,14 @@ const ScreenWrapper = styled.div`
 
 const Container = styled.div`
   width: 100%;
+  min-width: 80%;
   box-sizing: border-box;
   padding-inline: 2.875rem;
   padding-block: 68px;
   border-radius: 20px 20px 0 0;
   background-color: white;
   position: fixed;
-  bottom: -100%;
   transition: transform 600ms linear;
   bottom: 0;
   transform: translateY(${({ open }) => (open ? 0 : "100%")});
-`;
-
-const ButtonTest = styled.button`
-  position: absolute;
-  top: 28px;
-  left: 25px;
 `;
