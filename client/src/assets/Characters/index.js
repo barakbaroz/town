e0b235cf8ee0d08797cf872e335d95dae38b4b03
tomer255 <tomer255255@gmail.com>
2023-07-10 13:58:55 +0300

    Update assets

--- client/src/assets/Characters/index.js
+++ client/src/assets/Characters/index.js
@@ -1,36 +1,18 @@
-import female_0_2_white from "./female_0_2_white.svg";
-import female_0_2_black from "./female_0_2_black.svg";
-import male_0_2_white from "./male_0_2_white.svg";
-import male_0_2_black from "./male_0_2_black.svg";
-
-import female_3_8_white from "./female_3_8_white.svg";
-import female_3_8_black from "./female_3_8_black.svg";
-import male_3_8_white from "./male_3_8_white.svg";
-import male_3_8_black from "./male_3_8_black.svg";
-
-import female_9_18_white from "./female_9_18_white.svg";
-import female_9_18_black from "./female_9_18_black.svg";
-import male_9_18_white from "./male_9_18_white.svg";
-import male_9_18_black from "./male_9_18_black.svg";
-
+import female_20_50 from "./female_20_50.svg";
+import female_50_70 from "./female_50_70.svg";
+import female_70 from "./female_70.svg";
+import male_20_50 from "./male_20_50.svg";
+import male_50_70 from "./male_50_70.svg";
+import male_70 from "./male_70.svg";
 import blank from "./blank.svg";
 
 const avatars = {
-  "female_0-2_white": female_0_2_white,
-  "female_0-2_black": female_0_2_black,
-  "male_0-2_white": male_0_2_white,
-  "male_0-2_black": male_0_2_black,
-
-  "female_3-8_white": female_3_8_white,
-  "female_3-8_black": female_3_8_black,
-  "male_3-8_white": male_3_8_white,
-  "male_3-8_black": male_3_8_black,
-
-  "female_9-18_white": female_9_18_white,
-  "female_9-18_black": female_9_18_black,
-  "male_9-18_white": male_9_18_white,
-  "male_9-18_black": male_9_18_black,
-
+  female_20_50,
+  female_50_70,
+  female_70,
+  male_20_50,
+  male_50_70,
+  male_70,
   blank,
 };
 export default avatars;
