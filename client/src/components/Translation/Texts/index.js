56b3ef25e1de03d903b6dfc0a2a6907cf3994e2a
Tsachi <tsachi@gist.com>
2023-07-11 17:07:39 +0300

    Remove spanish according to design

--- client/src/components/Translation/Texts/index.js
+++ client/src/components/Translation/Texts/index.js
@@ -2,6 +2,5 @@ import he from "./Hebrew.json";
 import en from "./English.json";
 import ru from "./Russian.json";
 import ar from "./Arabic.json";
-import sp from "./Spanish.json";
 
-export const LanguagesData = { he, ru, ar, en, sp };
+export const LanguagesData = { he, ru, ar, en };
