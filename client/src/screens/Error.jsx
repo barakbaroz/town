fa46e80619d24fc3f231cc86523047c48f999c17
digmi123 <65775879+digmi123@users.noreply.github.com>
2023-07-16 14:02:43 +0300

    Add Error element component (Styling).

--- client/src/screens/Error.jsx
+++ client/src/screens/Error.jsx
@@ -1,5 +1,7 @@
 import { useRouteError, Navigate } from "react-router-dom";
 import NotFound from "./NotFound";
+import NotFoundLogo from "../assets/Logos/404Logo.svg";
+import styled from "styled-components";
 
 function ErrorElement() {
   const error = useRouteError();
@@ -7,7 +9,54 @@ function ErrorElement() {
   if (error.response?.statusText === "Forbidden")
     return <Navigate to="/login" />;
   if (error.response?.statusText === "Not Found") return <NotFound />;
-  return <h1>Error</h1>;
+  return (
+    <Page>
+      <Container>
+        <img src={NotFoundLogo} alt="not-found" />
+        <DescriptionContainer>
+          <Title>Opps…</Title>
+          <StatusDescription>something went wrong</StatusDescription>
+        </DescriptionContainer>
+        <ErrorText>Please try again latter</ErrorText>
+      </Container>
+    </Page>
+  );
 }
 
 export default ErrorElement;
+
+const Page = styled.div`
+  width: 100%;
+  height: 100%;
+  font-family: "Abraham";
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const DescriptionContainer = styled.div`
+  margin-block-start: 1.5rem;
+  margin-block-end: 2.875rem;
+`;
+
+const Title = styled.h1`
+  font-size: 2.625rem;
+  font-weight: 500;
+  margin: 0;
+`;
+const StatusDescription = styled.h3`
+  font-size: 1.5rem;
+  font-weight: 500;
+  margin: 0;
+`;
+const ErrorText = styled.div`
+  font-size: 1.25rem;
+`;
