841174cddf0b68391c49a6c8766f7f6f30bdd1e7
tomer255 <tomer255255@gmail.com>
2023-07-16 12:14:57 +0300

    Add errorElement to Panel

--- client/src/screens/Error.jsx
+++ client/src/screens/Error.jsx
@@ -1,9 +1,11 @@
-import { useRouteError } from "react-router-dom";
+import { useRouteError, Navigate } from "react-router-dom";
 import NotFound from "./NotFound";
 
 function ErrorElement() {
   const error = useRouteError();
-  console.log({ error });
+  console.log(error.response.statusText);
+  if (error.response?.statusText === "Forbidden")
+    return <Navigate to="/login" />;
   if (error.response?.statusText === "Not Found") return <NotFound />;
   return <h1>Error</h1>;
 }
