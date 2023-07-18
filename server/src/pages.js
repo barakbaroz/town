294f7ecbbc5182fa899a1f26e1b71d630d96367a
Tomer <tomer255@gmail.com>
2023-07-18 13:53:16 +0300

    Update pages.js

--- server/src/pages.js
+++ server/src/pages.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const path = require("path");
 const router = express.Router();
-
-router.get("/assets/*", (req, res) => {
-  res.sendFile(path.resolve(`./dist${req.path}`));
-});
+const fs = require("fs");
 
 router.get("/*", (req, res) => {
-  res
+  const requestedPath = fs.existsSync(`./dist${req.path}`)
+    ? req.path
+    : "/index.html";
+  return res
     .setHeader("Cache-Control", "no-cache, no-store, must-revalidate")
-    .sendFile(path.resolve(`./dist/index.html`));
+    .sendFile(path.resolve(`./dist${requestedPath}`));
 });
 
 module.exports = router;
