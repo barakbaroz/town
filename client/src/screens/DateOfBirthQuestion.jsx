e1357ae3a14a0096fb9c96699c48549e36c40839
digmi123 <65775879+digmi123@users.noreply.github.com>
2023-07-19 18:32:13 +0300

    Add pin input to Date of birth component.

--- client/src/screens/DateOfBirthQuestion.jsx
+++ client/src/screens/DateOfBirthQuestion.jsx
@@ -1,13 +1,19 @@
+import { useRef } from "react";
 import styled from "styled-components";
 import { Translator } from "../components/Translation";
 import AuthQuestionLayout from "../components/AuthQuestionLayout";
+import PinInput from "../components/Gister/PinInput";
 
 function DateOfBirthQuestion() {
+  const dateOfBirthInputRef = useRef(null);
+
   return (
     <AuthQuestionLayout index={2} key="DateOfBirth" nextRoute="Department">
       <Title>
         <Translator>מתי נולדת?</Translator>
       </Title>
+
+      <PinInput ZehutInputRef={zehutInputRef} onChange={handleZehutNumber} />
     </AuthQuestionLayout>
   );
 }
