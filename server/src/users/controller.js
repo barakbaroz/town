99e5f4f17270ca6a84db8c3a217bcd9a8c565cb2
tomer255 <tomer255255@gmail.com>
2023-07-20 13:53:17 +0300

    Add verify user route

--- server/src/users/controller.js
+++ server/src/users/controller.js
@@ -1,11 +1,28 @@
 const { isUUID } = require("../validator");
 const userServices = require("./service");
+const jwt = require("jsonwebtoken");
+
+module.exports.verify = async (req, res) => {
+  try {
+    const { id, zehutNumber, yearOfBirth } = req.body;
+    if (!isUUID(id)) return res.status(400).send("Error");
+    const user = await userServices.verify({ id, zehutNumber, yearOfBirth });
+    if (!user) return res.status(403).send("verification failed");
+    const token = jwt.sign({ id }, process.env.JWT_KEY_USER);
+    return res
+      .cookie("user_token", token, { httpOnly: true })
+      .status(200)
+      .send("Successfully verify");
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Error");
+  }
+};
 
 module.exports.getData = async (req, res) => {
   try {
-    const { userId } = req.body;
-    if (!isUUID(userId)) return res.status(400).send("Error");
-    const user = await userServices.getData({ userId });
+    const { id } = req.user;
+    const user = await userServices.getData({ userId: id });
     if (!user) return res.status(404).send("Not user found");
     return res.status(200).json(user);
   } catch (error) {
@@ -16,8 +33,8 @@ module.exports.getData = async (req, res) => {
 
 module.exports.update = async (req, res) => {
   try {
-    const { userId, ...data } = req.body;
-    if (!isUUID(userId)) return res.status(400).send("Error");
+    const userId = req.user.id;
+    const data = req.body;
     await userServices.update({ id: userId, data });
     return res.status(200).send("Updated");
   } catch (error) {
@@ -28,8 +45,8 @@ module.exports.update = async (req, res) => {
 
 module.exports.userAction = async (req, res) => {
   try {
-    const { userId: UserId, type, data } = req.body;
-    if (!isUUID(UserId)) return res.status(400).send("Error");
+    const UserId = req.user.id;
+    const { type, data } = req.body;
     await userServices.userAction({ UserId, type, data });
     return res.status(200).send("Updated");
   } catch (error) {
@@ -40,8 +57,8 @@ module.exports.userAction = async (req, res) => {
 
 module.exports.userVideoAction = async (req, res) => {
   try {
-    const { UserId, type, data } = req.body;
-    if (!isUUID(UserId)) return res.status(400).send("Error");
+    const UserId = req.user.id;
+    const { type, data } = req.body;
     await userServices.userVideoAction({ UserId, type, data });
     return res.status(200).send("Updated");
   } catch (error) {
