1b582fb6baf710f7b7ca71652230a604e6de2c53
Daniel-Dror <drordaniel10@gmail.com>
2023-07-20 19:02:48 +0300

    Add column to table

--- server/src/models/casesProgress.js
+++ server/src/models/casesProgress.js
@@ -11,6 +11,7 @@ const init = (sequelize) =>
       openSms: DataTypes.DATE,
       avatarSelection: DataTypes.DATE,
       watchedVideo: DataTypes.DATE,
+      feedbackAnswer: DataTypes.DATE,
     },
     { underscored: true }
   );
