5fd10a7a12fcec3502e84e3e2abcaf752679f874
tomer255 <tomer255255@gmail.com>
2023-07-20 13:49:45 +0300

    Add verifyToken to user

--- server/src/users/authorization.js
+++ server/src/users/authorization.js
@@ -1,14 +1,16 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-  const token = req.cookies.access_token;
+  const token = req.cookies.user_token;
+  console.log("verifyToken");
   if (!token) {
     return res.status(403).send("A token is required for authentication");
   }
   try {
-    req.staffMembers = jwt.verify(token, process.env.JWT_KEY_STAFF_MEMBERS);
+    req.user = jwt.verify(token, process.env.JWT_KEY_USER);
     return next();
   } catch (err) {
+    console.log(err);
     return res.status(401).send("Invalid Token");
   }
 };
