01f253b1f48b4fa6d65bfbb5dcb4e664f57aa953
tomer255 <tomer255255@gmail.com>
2023-07-20 16:15:15 +0300

    Remove log

--- server/src/users/authorization.js
+++ server/src/users/authorization.js
@@ -2,7 +2,6 @@ const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
   const token = req.cookies.user_token;
-  console.log("verifyToken");
   if (!token) {
     return res.status(403).send("A token is required for authentication");
   }
