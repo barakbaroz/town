235bd1b802f56501d26ca110452a7e3f885c78f2
digmi123 <65775879+digmi123@users.noreply.github.com>
2023-07-27 17:19:43 +0300

    Styling to card in new file.

---
+++ client/src/stylings/VideoPageStyling.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+  margin-inline: 25px;
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  border-radius: 15px;
+  padding-inline: 27px;
+  padding-block-start: 20px;
+  padding-block-end: 35px;
+`;
