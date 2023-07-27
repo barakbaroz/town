2523fdcc5c12aa833f439a9fe12511ac63d43f3f
digmi123 <65775879+digmi123@users.noreply.github.com>
2023-07-27 18:34:40 +0300

    Add general styling of icons to video page.

--- client/src/stylings/VideoPageStyling.jsx
+++ client/src/stylings/VideoPageStyling.jsx
@@ -9,4 +9,10 @@ export const Card = styled.div`
   padding-inline: 27px;
   padding-block-start: 20px;
   padding-block-end: 35px;
+  box-shadow: 0px 10px 8px #0000001f;
+`;
+
+export const Icon = styled.img`
+  max-width: 70px;
+  max-height: 70px;
 `;
