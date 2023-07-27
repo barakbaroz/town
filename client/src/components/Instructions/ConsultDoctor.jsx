40437ea434aa85adf56c2584f62094c8a522b2a2
digmi123 <65775879+digmi123@users.noreply.github.com>
2023-07-27 17:19:34 +0300

    Add new comnponents.

---
+++ client/src/components/Instructions/ConsultDoctor.jsx
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+import { Card } from "../../stylings/VideoPageStyling";
+import { Translator } from "../Translation";
+
+function ConsultDoctor() {
+  return (
+    <Card>
+      <TopSection>
+        <Title>
+          <Translator>התייעצות עם הרופא/ה המטפל/ת</Translator>
+        </Title>
+        <img src="" alt="image" />
+      </TopSection>
+
+      <Text>
+        <Translator>
+          עליך לפנות לרופא/ה מטפל/ת לצורך קבלת מרשם לחומרי ההכנה. במידה והינך
+          נוטל תרופות לטיפול בסוכרת ו/או נוגדות קרישה, יש לעקוב אחר ההמלצות
+          הפרטניות ולהביא לרופא/ה את הטופס להורדה המצורף כאן.
+        </Translator>
+      </Text>
+      <DoctorFormDownload>
+        <Translator>טופס לרופא/ת משפחה</Translator>
+      </DoctorFormDownload>
+    </Card>
+  );
+}
+
+export default ConsultDoctor;
+
+const TopSection = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-block-start: 1.25rem;
+  padding-block-end: 0.734rem;
+`;
+const Title = styled.h1`
+  margin: 0;
+  font-size: 1.375rem;
+`;
+
+const Text = styled.p`
+  margin: 0;
+  margin-block-end: 1.125rem;
+`;
+
+const DoctorFormDownload = styled.button`
+  --content-height: 1.625rem;
+  font-size: 1.125rem;
+  cursor: pointer;
+  color: #ffffff;
+  background-color: #f02a4c;
+  border: none;
+  padding-block: 0.688rem;
+  padding-inline: 27px;
+  border-radius: 50px;
+  align-self: center;
+  font-family: inherit;
+`;
