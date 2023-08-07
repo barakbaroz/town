bacebac495d682f3168b1f955f4c8e0e8114a728
digmi123 <65775879+digmi123@users.noreply.github.com>
2023-08-07 12:22:19 +0300

    Remove naviagtion arrows.

--- client/src/components/Questionnaire/Navigation.jsx
+++ client/src/components/Questionnaire/Navigation.jsx
@@ -1,25 +1,9 @@
-import navigationArrow from "../../assets/Icons/navigation_arrow.svg";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import PropTypes from "prop-types";
-import { useNavigate } from "react-router-dom";
 
-function Navigation({ nextQuestionKey, index, questionsSize }) {
-  const navigate = useNavigate();
-
-  const handleNext = () => {
-    if (nextQuestionKey) navigate(`../Questionnaire/${nextQuestionKey}`);
-  };
-  const handlePrev = () => {
-    navigate(-1);
-  };
+function Navigation({ index, questionsSize }) {
   return (
     <Container>
-      <ArrowIcon
-        src={navigationArrow}
-        alt="navigationArrow"
-        onClick={handleNext}
-        flip={true}
-      />
       <h3
         style={{
           paddingInline: "20%",
@@ -32,21 +16,13 @@ function Navigation({ nextQuestionKey, index, questionsSize }) {
         <span style={{ marginInline: "0.4rem" }}>/</span>
         {questionsSize}
       </h3>
-      <ArrowIcon
-        src={navigationArrow}
-        alt="navigationArrow"
-        onClick={handlePrev}
-        flip={false}
-      />
     </Container>
   );
 }
 
 Navigation.propTypes = {
-  maxQuestionIndex: PropTypes.number,
   index: PropTypes.number,
   questionsSize: PropTypes.number,
-  nextQuestionKey: PropTypes.string,
 };
 
 export default Navigation;
@@ -58,13 +34,4 @@ const Container = styled.div`
   justify-content: space-evenly;
   padding-block: 16px;
   padding-inline: var(--screen-padding-inline);
-`;
-
-const ArrowIcon = styled.img`
-  width: 0.8rem;
-  ${({ flip }) =>
-    flip &&
-    css`
-      transform: rotateY(180deg);
-    `}
 `;
