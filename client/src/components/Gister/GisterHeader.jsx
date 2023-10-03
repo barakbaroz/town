10dca4d3a4f7d325115af39d5f8baa942aa1dd64
Tsachi <tsachi@gist.com>
2023-10-03 17:45:57 +0300

    Add new hospital logo file to gister & searchBar

--- client/src/components/Gister/GisterHeader.jsx
+++ client/src/components/Gister/GisterHeader.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import gistLogo from "../../assets/Logos/gist_logo.svg";
-import hospitalLogo from "../../assets/Logos/hospital_logo.png";
+import hospitalLogo from "../../assets/Logos/hospital_logo_gister.svg";
 import PropTypes from "prop-types";
 
 function GisterHeader({ text }) {
