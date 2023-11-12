7bc0a3fbad793385d8b9073deb3a3b234e64df21
Barak <barozbarak@gmail.com>
2023-11-12 09:45:26 +0200

    Change login background and logo size

--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -133,7 +133,7 @@ const GistLogo = styled.img.attrs({ src: gistLogo })`
 
 const HopitalLogo = styled.img.attrs({ src: hospitalLogo })`
   position: absolute;
-  height: 71px;
+  height: 61px;
   top: 40px;
   right: 40px;
 `;
