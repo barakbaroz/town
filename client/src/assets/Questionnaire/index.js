326fb3a2d3d83ea6e64da67551f39cdd0010e2f1
Tsachi <tsachi@gist.com>
2023-11-23 11:34:29 +0200

    Replace cpap with blood pressure medicine

--- client/src/assets/Questionnaire/index.js
+++ client/src/assets/Questionnaire/index.js
@@ -1,13 +1,13 @@
 import diabetes_medicines from "./diabetes_medicines.svg";
 import blood_thinners from "./blood_thinners.svg";
-import cpap from "./cpap.svg";
+import bloodPressureMedicines from "./bloodPressureMedicines.svg";
 import nutritional_supplement from "./nutritional_supplement.svg";
 import colonoscopy_before from "./colonoscopy_before.jpg";
 
 const questionnaireImages = {
   diabetesMedicines: diabetes_medicines,
   bloodThinners: blood_thinners,
-  cpap: cpap,
+  bloodPressureMedicines: bloodPressureMedicines,
   nutritionalSupplement: nutritional_supplement,
   colonoscopyBefore: colonoscopy_before,
 };
