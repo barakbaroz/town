4f3335620824165fd5867e45a75ef0ccaad8a43c
tomer255 <tomer255255@gmail.com>
2024-01-11 10:24:04 +0200

    Change anme in add Staff Members

--- server/scripts/addStuffMembers.js
+++ server/scripts/addStuffMembers.js
@@ -4,5 +4,5 @@ const { StaffMembers } = require("../src/models");
 StaffMembers.create({
   email: "gist@gistmd.com",
   password: "123456",
-  name: "משתמש בדיקה",
+  name: "Test User",
 }).then(() => console.log("New user added"));
