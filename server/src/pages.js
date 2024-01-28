ec18842b0cb84a28b994299e0ff5b58773f50916
tomer255 <tomer255255@gmail.com>
2024-01-28 17:48:56 +0200

    Update serve page

--- server/src/pages.js
+++ server/src/pages.js
@@ -2,11 +2,14 @@ const express = require("express");
 const path = require("path");
 const router = express.Router();
 
+router.get("/", serve);
 router.use(express.static("./dist"));
-router.get("*", function (request, response) {
+router.get("*", serve);
+
+function serve(request, response) {
   return response
     .setHeader("Cache-Control", "no-cache, no-store, must-revalidate")
     .sendFile(path.resolve("./dist/index.html"));
-});
+}
 
 module.exports = router;
