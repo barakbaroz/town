28b060f5d005df4014816e4d81ce14480e987b39
tomer255 <tomer255255@gmail.com>
2024-02-05 12:32:17 +0200

    Update templates text and add Subject

--- server/src/reminders/templates.js
+++ server/src/reminders/templates.js
@@ -1,21 +1,15 @@
-module.exports = (type, user) => {
+module.exports.getMessage = (type, user) => {
   const { BASIC_URL } = process.env;
-  const startLink = `${BASIC_URL}/api/user/entry/${user.id}`;
-  const { procedureDate, procedureTime } = user.Case;
-  const date = new Date(procedureDate);
-  const threeDaysBefore = new Date();
-  threeDaysBefore.setDate(date.getDate() - 3);
+  const link = `${BASIC_URL}/api/user/entry/${user.id}`;
+  const { procedureDate } = user.Case;
+  const procedureDateString = formatDate(new Date(procedureDate), "date");
   const template = {
     key: "type",
-    CaseCreation: `שלום, לקראת הבדיקה שנקבעה לך לתאריך ${procedureDate} בשעה ${procedureTime} , חשוב לדעת שההכנה לבדיקה חיונית להצלחתה. הכנה בלתי מספקת עלולה להוביל לצורך בבדיקה חוזרת. בקישור הבא מצורף סרטון הדרכה מותאם אישית שיעזור לך להתכונן לבדיקה.\n${startLink}`,
-    FirstVideoReminder: `שלום, אנו מזכירים לך לצפות בסרטון ההדרכה לקראת הבדיקה שלך ולקבל מידע חשוב שיעזור לך להתכונן לבדיקה בצורה מיטבית.\n${startLink}`,
-    SecondVideoReminder: `שלום, זוהי תזכורת אחרונה לצפות בסרטון ההדרכה ולהגדיל משמעותית את הסיכוי שלך להגיע מוכן לבדיקה ולהימנע מצורך בבדיקה חוזרת.\n${startLink}`,
-    FirstNutritionReminder: `שלום, לקראת הבדיקה שלך ביוספטל בתאריך ${date.getDate()}/${
-      date.getMonth() + 1
-    } ב ${procedureTime} רצינו להזכיר לך שהחל מתאריך ${threeDaysBefore.getDate()}/${
-      threeDaysBefore.getMonth() + 1
-    } עליך לשנות את התזונה שלך ולאכול מזון דל סיבים בלבד לפי הפירוט בסרטון המצורף. הכנה טובה חיונית להצלחת הבדיקה!\n${startLink}`,
-    SecondNutritionReminder: `שלום, לקראת הבדיקה שלך בעוד יומיים ביוספטל, זאת תזכורת ש 24 שעות לפני הבדיקה שלך,  עליך לעבור לתזונה נוזלית בלבד ולהתחיל לקחת מרוקנים לניקוי המעי לפי הנחיית הרופא/ה. הכנה טובה חיונית להצלחת הבדיקה. יש ללחוץ על הקישור לקריאת ההנחיות.\n${startLink}`,
+    CaseCreation: `Hi! You have an examination scheduled for ${procedureDateString} at our clinic. It’s important that you understand how crucial correct preparation is for the procedure’s success. Incorrect bowel preparation may force you to redo the examination. Click on the link below to watch a personalized video that will help you prepare for the procedure.\n${link}`,
+    FirstVideoReminder: `Hi! Ahead of your examination, we remind you to watch the personalized video that will help you prepare better.\n${link}`,
+    SecondVideoReminder: `Hi! This is a final reminder to watch the personalized video and significantly increase your chances of being prepared for the examination and avoiding the need for a retake.\n${link}`,
+    FirstNutritionReminder: `Hi! Ahead of your examination at our clinic on ${procedureDateString}, we’d like to remind you that starting tomorrow, you need to change your diet and begin taking medication as instructed by your personal physician. Remember, correct bowel preparation is crucial for the examination’s success. Click on the link below to watch your personalized preparation instructions.\n${link}`,
+    SecondNutritionReminder: `Hi! Ahead of your examination, we remind you that In 24 hours, you need to stop eating solid food and begin taking laxatives as instructed by your personal physician. Correct bowel preparation is crucial for the examination’s success. Click on the link below \n${link}`,
   };
 
   return findMessageByParams(template, {
@@ -25,8 +19,17 @@ module.exports = (type, user) => {
   });
 };
 
-const defaultValues = {
-  gender: "male",
+module.exports.getSubject = (type) => {
+  const Subjects = {
+    CaseCreation: "Personalized videos for proper bowel preparation",
+    FirstVideoReminder: "We remind you to watch the video to prepare better!",
+    SecondVideoReminder:
+      "Final reminder to watch the bowel preparation instructions video",
+    FirstNutritionReminder: "Diet and medication adjustments",
+    SecondNutritionReminder:
+      "Taking your laxative solution and change to liquid diet",
+  };
+  return Subjects[type];
 };
 
 const formatOptions = {
@@ -41,9 +44,6 @@ const formatDate = (dateObj, type, locales = "en-US") => {
 
 const findMessageByParams = (template, userData) => {
   if (template instanceof Object && "key" in template)
-    return findMessageByParams(
-      template[userData[template.key]] || template[defaultValues[template.key]],
-      userData
-    );
+    return findMessageByParams(template[userData[template.key]], userData);
   return template;
 };
