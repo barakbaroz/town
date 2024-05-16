6eca7107e5fc807100dd91d526de4a386f274b10
Tsachi <tsachi@gist.com>
2024-05-16 10:12:58 +0300

    Fix CU-74

--- client/src/screens/Questionnaire.jsx
+++ client/src/screens/Questionnaire.jsx
@@ -124,7 +124,7 @@ const Answer = styled.label`
   width: 6.7rem;
   max-width: 45%;
   &:hover {
-    background-color: #bf213c;
+    background-color: #f02a4c;
   }
   &:has(${Input}:checked) {
     span {
