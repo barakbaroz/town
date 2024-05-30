5ef73f6b7a7ffd27e34d0c335682643473fcd167
Barak <barozbarak@gmail.com>
2024-05-30 11:28:35 +0300

    Handle http req  -SMS

--- server/src/reminders/sms.js
+++ server/src/reminders/sms.js
@@ -1,35 +1,46 @@
 const axios = require("axios");
-const { SMS_ACCOUNT_SID, SMS_AUTH_TOKEN, SMS_SENDER_NAME } = process.env;
+const {
+  SMS_ACCOUNT_SID,
+  SMS_AUTH_TOKEN,
+  SMS_SENDER_NAME,
+  SMS_FROM,
+  SMS_MESSAGE_TYPE,
+  DEVELOPMENT_TYPE,
+  SMS_API_KEY,
+} = process.env;
 const defaultSmsObj = {
   UserName: SMS_ACCOUNT_SID,
   Password: SMS_AUTH_TOKEN,
-  senderName: SMS_SENDER_NAME,
 };
-
-const testingNumbers = [
-  "0528033947",
-  "0546956695",
-  "0584236373",
-  "0524084538",
-  "0544659401",
-  "0512399319",
-];
 
 module.exports.send = async ({ message, phoneNumber }) => {
   if (!phoneNumber) return console.warn("No phone number provided to send Sms");
   if (!message) return console.warn("No message provided to send Sms");
-  const smsObj = {
-    ...defaultSmsObj,
-    BodyMessage: message,
-    Recipients: [
-      {
-        Cellphone: testingNumbers.includes(phoneNumber)
-          ? phoneNumber
-          : `+1${phoneNumber}`,
+  const developmentMapper = {
+    dev: {
+      obj_api: {
+        ...defaultSmsObj,
+        senderName: SMS_SENDER_NAME,
+        BodyMessage: message,
+        Recipients: [{ Cellphone: phoneNumber }],
       },
-    ],
+      url: "https://restapi.soprano.co.il/api/sms",
+    },
+    prod: {
+      obj_api: {
+        ...defaultSmsObj,
+        ApiKey: SMS_API_KEY,
+        From: SMS_FROM,
+        MessageType: SMS_MESSAGE_TYPE,
+        Template: message,
+        Contacts: [{ To: phoneNumber }],
+      },
+      url: "https://messenger.soprano.co.il/api/messages/send",
+    },
   };
-  const res = await axios.post("https://restapi.soprano.co.il/api/sms", smsObj);
+
+  const { obj_api, url } = developmentMapper[DEVELOPMENT_TYPE];
+  const res = await axios.post(url, obj_api);
   if (res.status != 200 || res.data.StatusCode !== 0)
     throw `Sms API response with ${res.data.StatusCode}`;
 };
